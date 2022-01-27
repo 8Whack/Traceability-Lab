58c48d049d6f165b685112704ad313255c0f63dd
8Whack <calvinpratt99@gmail.com>
2022-01-27 15:39:12 -0700

    required cors lol

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
  require('dotenv').config();
- const port = process.env.PORT || process.env.SERVER_PORT
+const cors = require('cors')
+ const port = process.env.PORT || 5678
  const app = express()
 
- app.get('/', (req, res) =>{
-     res.sendFile(path.join(__dirname, '/home.html'))
- })
-
  app.use(express.json());
- app.use(cors())
+ app.use(cors());
+ app.use(rollbar.errorHandler(0));
 
  // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -21,6 +19,11 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/home.html'))
+})
 
  app.listen(port, ()=>{
      console.log("listening on port " + port)
