007a88d68cecbdad0e78be41db57936db3a302d8
8Whack <calvinpratt99@gmail.com>
2022-01-27 16:00:13 -0700

    commented out rollbar

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const cors = require('cors')
 
  app.use(express.json());
  app.use(cors());
- app.use(rollbar.errorHandler(0));
+//  app.use(rollbar.errorHandler(0));
 
- // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '8db0cb5e59d14d309370c75d73bf0c38',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+//  // include and initialize the rollbar library with your access token
+// var Rollbar = require('rollbar')
+// var rollbar = new Rollbar({
+//   accessToken: '8db0cb5e59d14d309370c75d73bf0c38',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
 
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+// // record a generic message and send it to Rollbar
+// rollbar.log('Hello world!')
 
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, './Site/home.html'))
+    res.sendFile(path.join(__dirname, '/home.html'))
 })
 
  app.listen(port, ()=>{
