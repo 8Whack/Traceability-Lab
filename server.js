20cd21387309c017b02b258d7d624938974057c9
8Whack <calvinpratt99@gmail.com>
2022-01-27 15:27:59 -0700

    adding rollbar

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const path = require('path');
  })
 
  app.use(express.json());
+ app.use(cors())
+
+ // include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '<ServerAccessToken />',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
  app.listen(port, ()=>{
      console.log("listening on port " + port)
